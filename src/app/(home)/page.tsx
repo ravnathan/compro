56f0b656c38d1aceb9a2e90f316297f24cdec930
ravnathan <ravindranathandede@gmail.com>
2024-07-18 00:31:49 +0700

    modified

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,12 @@
 import Body from "@/components/body";
 import Hero from "@/components/hero";
 import Testimonials from "@/components/testimonials";
+import { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: "DeTea",
+  description: "Homepage"
+}
 
 export default function Home() {
   return (
