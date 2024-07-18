7cf528a70feda15f14f9221390173e237fbafe77
ravnathan <ravindranathandede@gmail.com>
2024-07-18 07:40:22 +0700

    modified

--- src/app/(home)/srvce/page.tsx
+++ src/app/(home)/srvce/page.tsx
@@ -12,7 +12,7 @@ export const metadata : Metadata = {
 export default function Services() {
     return (
         <div className="bg-green-950">
-            <Image src={'/greendetea.svg'} className="mx-auto object-cover bg-green-950" width={500} height={200} alt="Teacup header" />
+            <Image src={'/greendetea.svg'} className="mx-auto object-cover bg-green-950" width={200} height={200} alt="Teacup header" />
             <div className={arch.className}>
                 <h1 className="text-4xl text-center text-white">Our products & services</h1>
             </div>
